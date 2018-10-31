9451d30df67a4f52bbf98cb55139aab4eff2f3de
ant747 <ant747@gmail.com>
2018-10-31 05:19:09 +0300

    switch to pure python

--- mixed_copy.py
+++ mixed_copy.py
@@ -1,14 +1,23 @@
 import sys
 import shutil
+import io
+
 
 
 def mixed_copy(f_src, f_dest):
     assert len(f_src) <= 1024, 'Source path must contain <= 1024 symbols'
     assert len(f_dest) <= 3096, 'Destination path must contain <= 3096 symbols'
-    if sys.platform[:3] == 'win':
-        import win32file
+    if sys.platform == 'win32':
+        b = bytearray(16 * 1024 * 1024)
         prefix = '\\\\?\\'
-        win32file.CopyFile(prefix + f_src, prefix + f_dest, 0)
+        with io.open(prefix + f_src, "rb") as in_file:
+            with io.open(prefix + f_dest, "wb") as out_file:
+                while True:
+                    numread = in_file.readinto(b)
+                    if not numread:
+                        break
+                    out_file.write(b)
+        shutil.copymode(f_src, f_dest)
     else:
         shutil.copy(f_src, f_dest)
 
