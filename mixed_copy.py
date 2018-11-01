b1276671efbcdfa097534603fa48b5598c8a4fe1
ant747 <ant747@gmail.com>
2018-11-01 16:21:13 +0300

    Added unicode support for sys.argv for Windows

--- mixed_copy.py
+++ mixed_copy.py
@@ -1,13 +1,17 @@
 import sys
+if sys.platform == 'win32':
+    import win32_unicode_argv
 import os
 import shutil
 import io
-
+import pdb
 
 
 def mixed_copy(f_src, f_dest):
-    assert len(f_src) <= 1024, 'Source path must contain <= 1024 symbols'
-    assert len(f_dest) <= 3096, 'Destination path must contain <= 3096 symbols'
+    f_src = os.path.abspath(f_src)
+    f_dest = os.path.abspath(f_dest)
+    assert len(f_src) <= 1024, 'Absolute source path must contain <= 1024 symbols'
+    assert len(f_dest) <= 3096, 'Absolute destination path must contain <= 3096 symbols'
 
     if sys.platform == 'win32':
         if os.path.isdir(f_dest):
